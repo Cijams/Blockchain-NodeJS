8049e1885def1ec18bed93150c3aeed8ecf785f2
Christopher <Cocopelly255@gmail.com>
2019-04-06 18:09:22 -0700

    Added replaceChain method.
    Added a method to allow the blockchain to switch over its official record if someone presents it with a verified correct chain of longer computational length.

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,10 +1,19 @@
 const Block = require('./block');
 
+/**
+ * Class that links all of the different blocks together to become
+ * the blockchain.
+ */
 class Blockchain {
     constructor() {
         this.chain = [Block.genesis()];
     }
 
+    /**
+     * Adds a block to the blockchain.
+     * @param data
+     * @returns {Block}
+     */
     addBlock(data) {
         const block = Block.mineBlock(this.chain[this.chain.length-1], data);
         this.chain.push(block);
@@ -12,6 +21,15 @@ class Blockchain {
         return block;
     }
 
+    /**
+     * Checks to ensure that the chain passed to it is indeed a valid
+     * and legal blockchain. Start by ensuring the genesis block hash matches
+     * a fresh hash of its information. Next, iterate through the chain to make sure
+     * that each blocks last hash matches the recorded hash, and that the current hash
+     * matches a freshly hashed set.
+     * @param chain
+     * @returns {boolean}
+     */
     isValidChain(chain) {
         if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
         for (let i=1; i<chain.length; i++) {
@@ -25,6 +43,24 @@ class Blockchain {
             }
         }
         return true;
+}
+
+    /**
+     * Replaces the currently stored chain with the newest and longest version
+     * so long as all of the information is verified correct.
+     * @param newChain - The new chain that is to be tested.
+     */
+    replaceChain(newChain) {
+        if(newChain.length <= this.chain.length) {
+            console.log('Received chain is not longer than the current chain.');
+            return;
+        } else if (!this.isValidChain(newChain)) {
+            console.log('The received chain is not valid.');
+            return;
+        }
+
+        console.log('Replacing blockchain with the new chain');
+        this.chain = newChain;
     }
 }
 
